9feb93625bb052030c5b46e93068f781e7915748
optimalresource <joptimalresource@live.com>
2021-03-16 22:07:37 +0100

    Added a folder for template literals showing how new lines are written to the DOM

---
+++ Template Literals/index.js
@@ -0,0 +1,8 @@
+let greet = "Hello";
+let person = "Joseph";
+
+// get and set the inner text of the paragraph with an ID of content
+document.getElementById("content").innerText = `
+${greet}
+${person}
+`;
